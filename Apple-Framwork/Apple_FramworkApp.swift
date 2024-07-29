c3c775aea963d9ac0e75bb8ceb56c09abb3d1074
SabirAlam633 <akgileshgoswami@gmail.com>
2024-07-29 22:16:43 +0530

    Initial Commit

---
+++ Apple-Framwork/Apple_FramworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FramworkApp.swift
+//  Apple-Framwork
+//
+//  Created by APP-DEVELOPER on 29/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FramworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
